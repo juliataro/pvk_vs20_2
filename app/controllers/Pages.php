5978046f2e355fcdbc4431cf50c396dfe6019a5e
Julia <julia.taro@khk.ee>
2021-01-20 17:38:38 +0200

    Loodud ja võetud kasutusele vaade failid

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -9,14 +9,19 @@ class Pages extends Controller
      */
     public function __construct()
     {
-        echo 'Pages class is loaded <br>';
+        //echo 'Pages class is loaded <br>';
     }
     public function index(){
-        echo 'index method is loaded<br>';
-        $this->view('index');
+        //echo 'Index method is loaded<br>;
+        $data = array(
+            'title'=>'Welcome to Pages',
+            'content'=>'Pages index view is loaded by MVC'
+        );
+        $this->view('pages/index', $data);
     }
     public function about(){
-        echo 'about method is loaded<br>';
-        $this->view('about');
+        //echo 'about method is loaded<br>';
+        $data  = array('content' => 'Pages about view is loaded by MVC');
+        $this->view('pages/about', $data);
     }
 }
